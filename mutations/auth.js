1cfb0f2cff6ab197e2a91e7f6d40f45fdddd6fa9
saumyasinghal747 <saumyasmathtutoring@gmail.com>
2023-04-29 15:56:42 -0700

    email verification

--- mutations/auth.js
+++ mutations/auth.js
@@ -8,8 +8,8 @@ function getToken(uid) {
     return jwt.sign({ _id: uid }, process.env.JWT_SECRET, { expiresIn: '1h' })
 }
 
-function getVerificationToken(uid){
-    return jwt.sign({ _id: uid, mim: process.env.MIM_SECURITY }, process.env.JWT_SECRET, { })
+function getVerificationToken(uid) {
+    return jwt.sign({ _id: uid, mim: process.env.MIM_SECURITY }, process.env.JWT_SECRET, {})
 }
 
 async function getUser(token, client) {
@@ -23,15 +23,15 @@ async function getUser(token, client) {
     }
 }
 
-function hashPassword(plaintext){
+function hashPassword(plaintext) {
     return bcrypt.hashSync(plaintext, 10)
 }
 
-function checkPassword(plaintext, hash){
+function checkPassword(plaintext, hash) {
     return bcrypt.compareSync(plaintext, hash)
 }
 
-function createNewUser(email, username, password){
+function createNewUser(email, username, password) {
     return {
         email,
         passwordHash: hashPassword(password),
@@ -47,27 +47,36 @@ module.exports = {
         login: async (_, { email, password }, { dataSources: { users } }) => {
             const user = await users.getUserByEmail(email);
             if (user && checkPassword(password, user.passwordHash)) {
-                if (!user.emailVerified){
-                    return {error: "emailUnverified"}
+                if (!user.emailVerified) {
+                    return { error: "emailUnverified" }
                 }
-                return getToken(user._id)
+                return { token: getToken(user._id) }
             }
-            else return {error: "incorrectCredentials"}
+            else return { error: "incorrectCredentials" }
         },
         register: async (_, { email, password, username }, { dataSources: { users } }) => {
             const exists = await users.getUserByEmail(email);
             if (!!exists) {
                 console.log(exists)
-                return exists
+                return {error: "accountExists"}
             }
-            else {
-                const doc = createNewUser(email, username, password)
-                await client.db().collection("users").insertOne(doc)
-                // todo: send verification email
-                const verificationToken = getVerificationToken(doc._id)
-                sendVerificationEmail(doc.email, verificationToken, process.env.HOST)
-                return getToken(doc._id)
-            }
+            const doc = createNewUser(email, username, password)
+            await client.db().collection("users").insertOne(doc)
+            const verificationToken = getVerificationToken(doc._id)
+            await sendVerificationEmail(doc.email, verificationToken, process.env.HOST)
+            return {token: getToken(doc._id)}
+        },
+        verifyEmail: async (_, { token }, { dataSources: { users } }) => {
+            // decode the jwt
+            const { mim, _id } = jwt.verify(token, process.env.JWT_SECRET)
+            // check the MIM password
+            // if incorrect call error
+            if (mim !== process.env.MIM_SECURITY) { return { error: "tokenNotSecure" } }
+            // update the user's email verification status
+            await client.db().collection("users").updateOne({ _id }, { $set: { emailVerified: true } })
+            // generate a login token
+            return {token: getToken(_id)}
+            // send response
         }
     }
 }
